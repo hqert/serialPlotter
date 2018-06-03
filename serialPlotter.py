de50c89d71cab1cd1e38cf0bc951dbacb7e8c775
Loïs Bosson <lois.bosson@epfl.ch>
2018-06-03 13:36:09 +0200

    Live plotting ok, no parametrization yet

--- serialPlotter.py
+++ serialPlotter.py
@@ -15,10 +15,9 @@
 ###################################################################################
 ###################################################################################
 
-
-import matplotlib.pyplot as plt
-import matplotlib
-#import matplotlib.animation as animation
+import PySide
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
 import numpy as np
 from io import BytesIO
 import serial
@@ -100,10 +99,6 @@ class serialAcq:
 ###################################################################################
 # Animator
 # 
-# Manages the figure and animation state. Holds the axes for each subplot and 
-# an associated plotter object to update various parts of it. TODO: Formulation/check
-# callback list is 
-# given at init, each corresponding to a new subplot.
 #
 # TODO: Parametrize the plotters instantiated (via kwargs?)
 # TODO: Allow horizontal subplots/2D tiling
@@ -113,53 +108,47 @@ class serialAcq:
 
 class animator:
 
-    def __init__(self, plotterDictList, **kwargs):
-        self.fig, self.ax = plt.subplots(len(plotterDictList), 1)
+    def __init__(self, plotterDictList, title='Serial plotter',  **kwargs):
+        #QtGui.QApplication.setGraphicsSystem('raster')
+        app = QtGui.QApplication([])
+        mw = QtGui.QMainWindow()
+        mw.setWindowTitle(title)
+        cw = QtGui.QWidget()
+        mw.setCentralWidget(cw)
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+        cw.setLayout(grid)
+        mw.show()
         
         # Make it iterable no matter what, there must be a better way... TODO
         if len(plotterDictList) == 1:
             self.ax = [self.ax]
-        self.plotters = [ livePlot(plotterDict.pop('dataUpdate_cb'), ax, **plotterDict ) 
-                for plotterDict, ax in zip(plotterDictList, self.ax)]
- 
+        self.plotters = [ livePlot(plotterDict.pop('dataUpdate_cb'), grid, yPos, **plotterDict ) 
+                for plotterDict, yPos in zip(plotterDictList, range(len(plotterDictList)))]
+
+
+
+
+
+        # define a timer that will signal all plots to update
+        t = QtCore.QTimer()
+        for plotter in self.plotters:
+            t.timeout.connect(plotter.update)
         
-        self.updaters = [plotter.updateFig for plotter in self.plotters]
-        self.anim = matplotlib.animation.FuncAnimation(self.fig, self.updateAxes, interval = 1/24, blit=True) 
-        #Hack the drawing method to allow blitting of objects outside the axes bbox
-        matplotlib.animation.Animation._blit_draw = self._blit_draw
+        #start timer
+        t.start(50)
+        ## Start Qt event loop
+        if __name__ == '__main__':
+            import sys
+            if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+                QtGui.QApplication.instance().exec_()
+
 
 
 #    def stopAnimation(self):
 #        if not self.running:
 #            print('stop!')
-#            self.anim.event_source.stop()
 
-
-    def updateAxes(self, frame = 0): 
-        artists = []
-        for updater in self.updaters:
-            for plotArtist in updater(frame):
-                artists.append(plotArtist)
-        return artists
-
-
-    # Redefinition of the drawing routine from Stack Overflow question "Animated Title in Matplotlib"
-    def _blit_draw(self, artists, bg_cache):
-        # Handles blitted drawing, which renders only the artists given instead
-        # of the entire figure.
-        updated_ax = []
-        for a in artists:
-            # If we haven't cached the background for this axes object, do
-            # so now. This might not always be reliable, but it's an attempt
-            # to automate the process.
-            if a.axes not in bg_cache:
-                bg_cache[a.axes] = a.figure.canvas.copy_from_bbox(a.axes.figure.bbox)
-            a.axes.draw_artist(a)
-            updated_ax.append(a.axes)
-
-        # After rendering all the needed artists, blit each axes individually.
-        for ax in set(updated_ax):
-            ax.figure.canvas.blit(ax.figure.bbox)
 
 
 
@@ -171,18 +160,16 @@ class animator:
 # There may be more legend entries than arrays returned by the updater callback
 #
 # TODO: implement automatic number of lines
-# TODO: switch to something else than matplotlib, the thing is terribly slow
-#       and glitchy with scrolling axes hack
 # 
 ###################################################################################
 
 class livePlot:
 
 
-    def __init__(self, updateData_cb, ax, **kwargs):
+    def __init__(self, updateData_cb, grid, yPos, **kwargs):
 
         defaults = {
-            'windowSize'    : 500 ,
+            'windowSize'    : 500,
             'showLegend'    : True,
             'labels'        : ['poney 1', 'poney 2'],
             'autoResizeY'   : False,
@@ -191,7 +178,7 @@ class livePlot:
             'maxY'          : 500,
             'title'         : '',
         }
-        
+        print(yPos) 
         # Use kwargs if available, defaults if not given
         for key in defaults:
             self.__setattr__(key, kwargs.pop(key, defaults[key]))
@@ -203,53 +190,24 @@ class livePlot:
         
         self.updateData_cb = updateData_cb
 
-        # Init plotting
-        self.ax     = ax
-        self.ax.set_title(self.title)
-        self.maxX   = self.windowSize
-        self.dataX  = [0]
-        self.dataY  = [ [0] for _ in range(len(self.labels)) ]
-        
-        self.lines  = []
-        for Y, legend in zip(self.dataY, self.labels):
-            line = ax.plot(self.dataX, Y, label=legend)  # Returns a list of only one Line2D
-            self.lines.append(line[0])
+        pw1 = pg.PlotWidget()
+        grid.addWidget(pw1, yPos, 0)
+        raw1 = QtGui.QLabel('test {}'.format(yPos))
+        raw1.setMinimumWidth(200)
+        grid.addWidget(raw1, yPos, 1)
+        p1 = pw1.plot([1, 2, 3, 4, 5, 4, 6, 7, 2, 8])
+        pw1.setTitle(self.title);
+        pw1.setLabel('left', 'Value', units='V')
+        pw1.setLabel('bottom', 'Time', units='s')
+        pw1.setXRange(0, self.windowSize)
+        pw1.setYRange(-10, 300)
+        self.p1 = p1
 
-        self.ax.set_xlim(0, self.windowSize)
-        self.ax.set_ylim(self.minY, self.maxY) # Will be resized later if needed
-
-        self.ax.legend().set_visible(self.showLegend)
-        
-    
-    def updateFig(self, frame = 0): #frame number given by matplotlib's animation, useless here
+    def update(self):
+        print('updating')
         data = np.array(self.updateData_cb())
-        self.dataX = data[0]
-        self.dataY = data[1:]
-        for line, Y in zip(self.lines, self.dataY):
-            line.set_data(self.dataX, Y)
-         
-        # Default artists to be updated
-        artists = []
-        for a in self.lines:
-            artists.append(a)
-        
-        # Y axis
-        if self.autoResizeY:
-            rangeY = (np.nanmin(self.dataY), np.nanmax(self.dataY))
-            #resize only if needed
-            if rangeY != self.ax.get_ylim():
-                self.ax.set_ylim(rangeY)
-            artists.append(self.ax.yaxis)
+        self.p1.setData( y = data[1])
 
-        #X axis
-        if self.XScroll:
-            self.maxX = np.max([self.windowSize, self.dataX[-1]])
-            self.ax.set_xlim(self.maxX - self.windowSize, self.maxX)
-            artists.append(self.ax.xaxis)
-
-        return artists
-        
-   
 
 
 
@@ -301,7 +259,7 @@ class dataProcessor:
 def dataFilter(dataIn):
 #    dataOut = np.convolve(dataIn, [1/5]*5, mode='same')
     dataOut = [np.NaN for _ in range(len(dataIn))] # Init the array
-    averageLen = 100
+    averageLen = 200
     for i in range(len(dataOut) - averageLen):
         dataOut[i] = dataIn[i: i+averageLen].sum() / averageLen
     
@@ -314,27 +272,27 @@ def dataFilter(dataIn):
 if __name__ == '__main__':
     print("Using python: " + platform.python_version())
     print("Using numpy: " + np.__version__)
-    print("Using matplotlib: " + matplotlib.__version__)
+#    print("Using matplotlib: " + matplotlib.__version__)
     print("Serial: " + serial.__version__)
 
 
     # Serial
     acquisition = serialAcq(
-                            port            = '/dev/ttyACM4',
+                            port            = '/dev/ttyACM0',
                             XChan           = False,
                             replaceNaNs     = True,
                             bufferLength    = 500,
                         )
     acquisition.updateData()
-    
-    
+     
+     
     # Processing
 
     filtering = dataProcessor(
             acquisition.updateData,
             [dataFilter]*2,
-            outputRaw = True)
-
+            outputRaw = False)
+ 
     # Animator
     live = animator([
        {
@@ -358,5 +316,16 @@ if __name__ == '__main__':
             'minY'          : -20,
             'maxY'          : 500,
             'title'         : 'Filtered data',
+            },
+        {
+            'dataUpdate_cb' : filtering.process,
+            'windowSize'    : 500,
+            'showLegend'    : True,
+            'labels'        : ['Poney 1', 'Poney 2', 'Cheval 1', 'Cheval 2'],
+            'autoResizeY'   : False,
+            'XScroll'       : True,
+            'minY'          : -20,
+            'maxY'          : 500,
+            'title'         : 'Flow',
             }
-        ])
+        ], title='Lit 2018 - Team 1')
